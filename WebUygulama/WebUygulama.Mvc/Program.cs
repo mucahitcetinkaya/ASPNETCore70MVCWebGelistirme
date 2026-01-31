fc2bafd8417be8bdcc851d2175535b68893c7ed3
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2026-01-31 16:22:51 +0300

    B5 - VİDEO 14 DbContext Sınıfını Kullanarak Veritabanı ve Entity Model Arasında Köprü Kurma

--- WebUygulama/WebUygulama.Mvc/Program.cs
+++ WebUygulama/WebUygulama.Mvc/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebUygulama.Mvc.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<UygulamaDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -40,3 +45,98 @@ controller dedigin Controller klasöründeki HomeController.cs
 action dediði Controller klasöründeki Index metotu
 
  */
+
+
+/*
+ 
+ VÝDEO 14. DbContext Sýnýfýný Kullanarak Veritabaný ve Entity Model Arasýnda Köprü Kurma
+ 
+DBContext sýnýfýna bakacagýz 
+
+nedir
+
+veritabanýmýz var sql server 
+ve bizim entitylerimiz olacak modellerimiz KitapTuru bir model entity oluyor aslýnda 
+
+karsýlýgý veri tabanýnda tablo olan class lara entity diyoruz 
+
+veritabaný ile entity arasýndaki köprüsü dbcontext ile kuruyoruz
+
+DbContext ten bir sýnýf olusturun sonra bundan nesne olusturup entity ve veri tabaný arasýndaki baglantýlarý yapabilirsiniz
+
+WebUygulama.Mvc içinde yeni bir klasör olustur
+ 
+oraya gidelim
+
+UygulamaDbContext ten geldik
+
+DbContext i buraya servis olarak eklememýz lazým 
+
+builder.Services.AddDbContext<UygulamaDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Defaultconnection")));
+
+bunu ekledik buraya
+
+bu nerden geldi appsettings.json da ekleme yapmýstýk 
+
+,
+  "ConnectionStrings": {
+    "DefaultConnection": "Server=SQLSERVERNAME;DataBase=DBADI;Trusted_Connection=True;TrustServerCertificate=True"
+  }
+}
+
+ConnectionStrings in ustune gelince yazýyor orda Get.ConnectionString(string)
+
+AddDbContext DbContext i kullan diyoruz nesnesini veriyoruz
+bunu kullanýrkende sqlserver kullan diyoruz
+sqlserver kullanýrkende json içindeki Defaultconnection i kullan diyoruz
+
+db i kuralým þimdi 
+
+tools dan nuget package maganer 
+package maganer console
+
+update-database
+
+PM> update-database
+Build started...
+Build succeeded.
+Microsoft.EntityFrameworkCore.Database.Command[20101]
+      Executed DbCommand (124ms) [Parameters=[], CommandType='Text', CommandTimeout='60']
+      CREATE DATABASE [VTKitapSatis];
+Microsoft.EntityFrameworkCore.Database.Command[20101]
+      Executed DbCommand (66ms) [Parameters=[], CommandType='Text', CommandTimeout='60']
+      IF SERVERPROPERTY('EngineEdition') <> 5
+      BEGIN
+          ALTER DATABASE [VTKitapSatis] SET READ_COMMITTED_SNAPSHOT ON;
+      END;
+Microsoft.EntityFrameworkCore.Database.Command[20101]
+      Executed DbCommand (4ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+      SELECT 1
+Microsoft.EntityFrameworkCore.Database.Command[20101]
+      Executed DbCommand (9ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+      CREATE TABLE [__EFMigrationsHistory] (
+          [MigrationId] nvarchar(150) NOT NULL,
+          [ProductVersion] nvarchar(32) NOT NULL,
+          CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+      );
+Microsoft.EntityFrameworkCore.Database.Command[20101]
+      Executed DbCommand (0ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+      SELECT 1
+Microsoft.EntityFrameworkCore.Database.Command[20101]
+      Executed DbCommand (8ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+      SELECT OBJECT_ID(N'[__EFMigrationsHistory]');
+Microsoft.EntityFrameworkCore.Database.Command[20101]
+      Executed DbCommand (14ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+      SELECT [MigrationId], [ProductVersion]
+      FROM [__EFMigrationsHistory]
+      ORDER BY [MigrationId];
+Microsoft.EntityFrameworkCore.Migrations[20405]
+      No migrations were applied. The database is already up to date.
+No migrations were applied. The database is already up to date.
+Done.
+PM> 
+
+olusturdu sorun yaratmadý
+
+
+*/
---
+++ WebUygulama/WebUygulama.Mvc/Utility/UygulamaDbContext.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace WebUygulama.Mvc.Utility
+{
+    public class UygulamaDbContext:DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options){}
+    }
+}
+
+
+/*
+ 
+ 14. video program.cs den geldik
+
+ilk hali
+
+namespace WebUygulama.Mvc.Utility
+{
+    public class UygulamaDbContext
+    {
+    }
+}
+
+namespace WebUygulama.Mvc.Utility
+{
+    public class UygulamaDbContext:DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options){}
+    }
+}
+
+miras aldı dbcontext i 
+yeni bir nesne yarattık
+constrator olusturduk 
+
+burası mekanizma asp.net in sistemi böyle calısıyor
+
+program.cs e gidelim 
+ 
+ 
+ */
