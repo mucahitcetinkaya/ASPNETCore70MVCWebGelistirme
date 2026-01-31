eb0dbecec37193415ef89337fbd8c53ebf096114
Mücahit Çetinkaya <mucahitcetinkaya@outlook.com>
2026-01-31 11:16:16 +0300

    Bölüm 5 MVC Mimarimizin Model Katmanının Entity Framework Kullanarak Oluşturulması - Video 12 Model Katmanına İlk Sınıfı Ekliyoruz

---
+++ WebUygulama/WebUygulama.Mvc/Models/KitapTuru.cs
@@ -0,0 +1,22 @@
+﻿namespace WebUygulama.Mvc.Models
+{
+    public class KitapTuru
+    {
+        public int ID { get; set; }
+        public string Ad { get; set; }
+
+    }
+}
+
+
+
+/*
+ 
+ilk sınıfımızı ekledik
+
+sınıfta prop ları ekleyelim bunlar sql de db de sutun olarak gelecekler entityframework ile 
+
+şimdilik bu kadar yeter bunlar sql de sutun olarak gelecekler
+
+ 
+ */
